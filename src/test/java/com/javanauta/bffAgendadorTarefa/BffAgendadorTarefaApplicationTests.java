cfefd1d2d4fb5f0941ae97c49541da02093bdeca
Eduardo Soares <eduardo_soares001@outlook.com>
2026-02-10 12:55:18 -0300

    commit inicial

---
+++ src/test/java/com/javanauta/bffAgendadorTarefa/BffAgendadorTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.bffAgendadorTarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
